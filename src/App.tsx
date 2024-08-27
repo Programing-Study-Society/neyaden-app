b81d14dceed07ebc0ca31bdd949c197b075d1d84
shiryu1202 <gp22a030@oecu.jp>
2024-08-28 03:02:06 +0900

    「遅延発生時に、遅延なしの時間に取り消しを引く処理」の見た目の作成が完了

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,120 @@ import { invoke } from "@tauri-apps/api/tauri";
 import "./App.css";
 
 function App() {
+  return (
+    <div>
+      {/* ヘッダー */}
+      <header>
+        <div className="flex border">
+          <div>
+            <div className="flex">
+              <h2>寝屋川市駅</h2>
+              <h2>電車状況</h2>
+            </div>
+            <div className="flex">
+              <p>9:50</p>
+              <p>更新</p>
+            </div>
+          </div>
+          <div className="flex right now-time">
+            <h1>現在</h1>
+            <h1>9:53</h1>
+          </div>
+        </div>
+      </header>
+
+      <body>
+        {/* 淀屋橋・中之島線方面 */}
+        <div className="border">
+          <div className="flex-right">
+            <h1>淀屋橋・中之島線</h1>
+            <h3>方面</h3>
+          </div>
+          {/* 1電車目 */}
+          <div>
+            <div className="flex">
+              <h2>10:00</h2>
+              <h2>発</h2>
+              <h2 className="orange">急行</h2>
+              <h2>淀屋橋行き</h2>
+              <h2 className="red display-none">遅延5分</h2>
+            </div>
+            <div className="flex">
+              <div className="flex display-none">
+                <h2 className="top red">10:00</h2>
+                <h2 className="top red">発</h2>
+              </div>
+              <h2 className="right red">走れば間に合います</h2>
+            </div>
+          </div>
+
+          {/* 2電車目 */}
+          <div>
+            <div className="flex margin">
+              <h2>10:05</h2>
+              <h2>発</h2>
+              <h2 className="blue">準急</h2>
+              <h2>淀屋橋行き</h2>
+              <h2 className="red">遅延5分</h2>
+            </div>
+            <div className="flex">
+              <div className="flex">
+                <h2 className="top red">10:10</h2>
+                <h2 className="top red">発</h2>
+              </div>
+              <h2 className="right yellow">歩きで間に合います</h2>
+            </div>
+          </div>
+        </div>
+
+        {/* 三条・出町柳方面 */}
+        <div>
+          <div className="flex-right margin">
+            <h1>三条・出町柳</h1>
+            <h3>方面</h3>
+          </div>
+          {/* 1電車目 */}
+          <div>
+            <div className="flex">
+              <h2>9:55</h2>
+              <h2>発</h2>
+              <h2 className="white">普通</h2>
+              <h2>三条行き</h2>
+              <h2 className="red display-none">遅延5分</h2>
+            </div>
+            <div className="flex">
+              <div className="flex display-none">
+                <h2 className="top red">10:00</h2>
+                <h2 className="top red">発</h2>
+              </div>
+              <h2 className="right yellow">歩きで間に合います</h2>
+            </div>
+          </div>
+
+          {/* 2電車目 */}
+          <div>
+            <div className="flex margin">
+              <h2>9:45</h2>
+              <h2>発</h2>
+              <h2 className="white">普通</h2>
+              <h2>淀屋橋行き</h2>
+              <h2 className="red">遅延15分</h2>
+            </div>
+            <div className="flex">
+              <div className="flex">
+                <h2 className="top red">10:00</h2>
+                <h2 className="top red">発</h2>
+              </div>
+              <h2 className="right red">走れば間に合います</h2>
+            </div>
+          </div>
+        </div>
+      </body>
+    </div>
+  );
+}
+
+function exampleApp() {
   const [greetMsg, setGreetMsg] = useState("");
   const [name, setName] = useState("");
 
