924c03cc95663233fd9e97e1f90f3e29d2761f80
harusann <gp22a074@oecu.jp>
2024-08-28 21:38:24 +0900

    今走っている電車の中で間に合う電車を取得可能に

--- src-tauri/src/train_info/stab_get_train_info.rs
+++ src-tauri/src/train_info/stab_get_train_info.rs
@@ -1,8 +1,8 @@
 use super::get_train_info::{ArrivalInfo, TrainInfo};
 
 #[tauri::command]
-pub fn get_train_info() -> TrainInfo {
-	TrainInfo {
+pub fn success_get_train_info() -> Result<TrainInfo, String> {
+	Ok(TrainInfo {
 		update_time: String::from("2024-08-26T09:30+09:00"),
 		yodoyabashi_direction: vec![
 			ArrivalInfo {
@@ -10,6 +10,7 @@ pub fn get_train_info() -> TrainInfo {
 				real_departure_time: String::from("2024-08-26T09:45:00+09:00"),
 				train_type: String::from("急行"),
 				terminal_station: String::from("淀屋橋"),
+				train_direction: String::from("0"),
 				is_delayed: false,
 				delay_time: String::from("00:00"),
 				travel_mode: String::from("走り"),
@@ -19,6 +20,7 @@ pub fn get_train_info() -> TrainInfo {
 				real_departure_time: String::from("2024-08-26T09:55:00+09:00"),
 				train_type: String::from("準急"),
 				terminal_station: String::from("中之島"),
+				train_direction: String::from("0"),
 				is_delayed: true,
 				delay_time: String::from("00:05"),
 				travel_mode: String::from("歩き"),
@@ -30,6 +32,7 @@ pub fn get_train_info() -> TrainInfo {
 				real_departure_time: String::from("2024-08-26T09:42:00+09:00"),
 				train_type: String::from("普通"),
 				terminal_station: String::from("出町柳"),
+				train_direction: String::from("1"),
 				is_delayed: true,
 				delay_time: String::from("00:27"),
 				travel_mode: String::from("走り"),
@@ -39,10 +42,16 @@ pub fn get_train_info() -> TrainInfo {
 				real_departure_time: String::from("2024-08-26T09:45:00+09:00"),
 				train_type: String::from("普通"),
 				terminal_station: String::from("三条"),
+				train_direction: String::from("1"),
 				is_delayed: false,
 				delay_time: String::from("00:00"),
 				travel_mode: String::from("走り"),
 			},
 		],
-	}
+	})
+}
+
+#[tauri::command]
+pub fn failed_get_train_info() -> Result<TrainInfo, String> {
+	Err(String::from("情報の取得に失敗しました"))
 }
